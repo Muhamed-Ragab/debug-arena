164c6bc532bd7cc5f2c17917a7e6d690d069f80e
Muhamed-Ragab <mohamedragab1649@gmail.com>
2026-08-23 19:58:57 +0300

    feat(api): add global logger, exception filter, and response interceptor

---
+++ apps/api/src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,15 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+
+export interface ApiSuccessResponse<T> {
+  success: true;
+  data: T;
+}
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, ApiSuccessResponse<T>> {
+  intercept(_context: ExecutionContext, next: CallHandler<T>): Observable<ApiSuccessResponse<T>> {
+    return next.handle().pipe(map((data: T) => ({ success: true, data })));
+  }
+}
