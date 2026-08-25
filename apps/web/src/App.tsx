71e0ead44d1c85547509d610b528b0ec83a2c845
Muhamed-Ragab <mohamedragab1649@gmail.com>
2026-08-25 08:42:15 +0300

    feat(web): better-auth client and protected routes

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import AppShell from "./components/layout/AppShell";
+import ProtectedRoute from "./components/ProtectedRoute";
 import LandingPage from "./features/landing/LandingPage";
 import LoginPage from "./features/auth/LoginPage";
 import RegisterPage from "./features/auth/RegisterPage";
@@ -18,13 +19,15 @@ export default function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-      <Route element={<AppShell />}>
-        <Route path="/challenges" element={<ChallengeBrowserPage />} />
-        <Route path="/challenges/:id" element={<ChallengePage />} />
-        <Route path="/submissions/:id/results" element={<ResultsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/settings" element={<ProfileSettingsPage />} />
-        <Route path="/leaderboard" element={<LeaderboardPage />} />
+      <Route element={<ProtectedRoute />}>
+        <Route element={<AppShell />}>
+          <Route path="/challenges" element={<ChallengeBrowserPage />} />
+          <Route path="/challenges/:id" element={<ChallengePage />} />
+          <Route path="/submissions/:id/results" element={<ResultsPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/settings" element={<ProfileSettingsPage />} />
+          <Route path="/leaderboard" element={<LeaderboardPage />} />
+        </Route>
       </Route>
     </Routes>
   );
